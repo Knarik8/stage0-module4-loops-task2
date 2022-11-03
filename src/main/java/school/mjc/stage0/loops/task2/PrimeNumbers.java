bc5f67144a2f86af5fb1be409d2ea940fb2bf13c
Armine8 <armisha15@mail.ru>
2022-11-03 18:33:42 +0300

    Task #2. While Loops

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,22 +1,24 @@
 package school.mjc.stage0.loops.task2;
 
-import javax.net.ssl.SSLContext;
-
 public class PrimeNumbers {
     public static void printPrimeNumbers(int printToInclusive) {
         // ПЧ- это число которое делится толко на себя и на 1
-        int counter = 5; // 3, 4, 5,
-        int divider = 2; // 2, 3, 4, 5
+        int low = 2;
 
-        while (counter <= printToInclusive) {
-            while (divider < counter) {
-                if (counter % divider == 0) {
-                    counter++;
-                } else if (counter % divider != 0) {
-                    divider++;
-                    System.out.println(counter);
+        while (low < printToInclusive) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
                 }
             }
+            if (!flag && low != 0 && low != 1)
+                System.out.print(low + " ");
+
+            ++low;
         }
     }
     public static void main(String[] args){
